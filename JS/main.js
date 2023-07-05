d80be2b1dee0e6642aafd1eba5ec6e3913f73d5b
leandrolalanne <leandrolalanne@gmail.com>
2023-07-05 14:42:14 -0300

    Primera prueba de html google forms

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+var sheet = SpreadsheetApp.openById("18R1BjaVcMJKb-kboTAOVEJoVPUs3AWO5eevW1p_0-_0");
+var sheetRange = sheet.getRange("A1");
+sheetRange.setValue("The string value");
+
+var file = Utilities.newBlob(fileData);
+var sheet = SpreadsheetApp.openById("18R1BjaVcMJKb-kboTAOVEJoVPUs3AWO5eevW1p_0-_0");
+var sheetRange = sheet.getRange("A1");
+sheetRange.setValue(file);
+
+function onsubmit() {
+    var formData = new FormData(this);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://script.google.com/u/0/home/projects/1TKd8T12ZoGBFj76AxNROSpf_e6EWHLPHIFVUuE9lM1ioobf_MwjstRTR/exec");
+    xhr.send(formData);
+}
